1bb55e0269705148aa519f16da6647b5c3eb66a1
rushabh-exe <rushabhmevada63@gmail.com>
2023-09-08 00:45:15 +0530

    minor changes

--- src/components/Band.jsx
+++ src/components/Band.jsx
@@ -8,62 +8,64 @@ function Band() {
       <main>
       </main>
       <div className="main2">
-        <div className="card">
-          <div className="card-image"></div>
-          <div>
-            <div className="category">Illustration</div>
-            <div className="heading">
-              A heading that must span over two lines
-              <div className="author">
-                By <span className="name">Abi</span> 4 days ago
+        <div className='card-container'>
+          <div className="card">
+            <div className="card-image"></div>
+            <div>
+              <div className="category">Illustration</div>
+              <div className="heading">
+                A heading that must span over two lines
+                <div className="author">
+                  By <span className="name">Abi</span> 4 days ago
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="card">
-          <div className="card-image"></div>
-          <div>
-            <div className="category">Illustration</div>
-            <div className="heading">
-              A heading that must span over two lines
-              <div className="author">
-                By <span className="name">Abi</span> 4 days ago
+          <div className="card">
+            <div className="card-image"></div>
+            <div>
+              <div className="category">Illustration</div>
+              <div className="heading">
+                A heading that must span over two lines
+                <div className="author">
+                  By <span className="name">Abi</span> 4 days ago
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="card">
-          <div className="card-image"></div>
-          <div>
-            <div className="category">Illustration</div>
-            <div className="heading">
-              A heading that must span over two lines
-              <div className="author">
-                By <span className="name">Abi</span> 4 days ago
+          <div className="card">
+            <div className="card-image"></div>
+            <div>
+              <div className="category">Illustration</div>
+              <div className="heading">
+                A heading that must span over two lines
+                <div className="author">
+                  By <span className="name">Abi</span> 4 days ago
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="card">
-          <div className="card-image"></div>
-          <div>
-            <div className="category">Illustration</div>
-            <div className="heading">
-              A heading that must span over two lines
-              <div className="author">
-                By <span className="name">Abi</span> 4 days ago
+          <div className="card">
+            <div className="card-image"></div>
+            <div>
+              <div className="category">Illustration</div>
+              <div className="heading">
+                A heading that must span over two lines
+                <div className="author">
+                  By <span className="name">Abi</span> 4 days ago
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="card">
-          <div className="card-image"></div>
-          <div>
-            <div className="category">Illustration</div>
-            <div className="heading">
-              A heading that must span over two lines
-              <div className="author">
-                By <span className="name">Abi</span> 4 days ago
+          <div className="card">
+            <div className="card-image"></div>
+            <div>
+              <div className="category">Illustration</div>
+              <div className="heading">
+                A heading that must span over two lines
+                <div className="author">
+                  By <span className="name">Abi</span> 4 days ago
+                </div>
               </div>
             </div>
           </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,19 +4,19 @@ function Footer() {
   return (
     <footer>
         <div className="parts">
-         <a href=""> <img src="./assets/facebook.png"  alt="facebook" /></a>
-         <a href=""> <img src="./assets/instagram.png"  alt="instagram" /></a>
-         <a href=""> <img src="./assets/twitter.png"  alt="twitter" /></a>
-         <a href=""> <img src="./assets/whatsapp.png"  alt="whatsapp" /></a>
+         <a href="/"> <img src="./assets/facebook.png"  alt="facebook" /></a>
+         <a href="/"> <img src="./assets/instagram.png"  alt="instagram" /></a>
+         <a href="/"> <img src="./assets/twitter.png"  alt="twitter" /></a>
+         <a href="/"> <img src="./assets/whatsapp.png"  alt="whatsapp" /></a>
         </div>
 
         <div className="parts">
           <ul>
-            <li><a href="#">Contact us</a></li>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Join us</a></li>
-            <li><a href="#">Terms & Conditions</a></li>
-            <li><a href="#">Join Whatsapp Group</a></li>
+            <li><a href="/">Contact us</a></li>
+            <li><a href="/">About us</a></li>
+            <li><a href="/">Join us</a></li>
+            <li><a href="/">Terms & Conditions</a></li>
+            <li><a href="/">Join Whatsapp Group</a></li>
           </ul>
         </div>
 
